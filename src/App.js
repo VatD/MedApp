873703ae9934cb919f7003420500c7e1dc63215c
Vatsal <dhar.raja001@gmail.com>
2021-03-07 16:12:35 +0530

    added login, changed auth context, minor bug fixes

--- src/App.js
+++ src/App.js
@@ -7,20 +7,11 @@ import './assets/scss/argon-dashboard-react.scss';
 import AdminLayout from './layouts/Admin.js';
 import AuthLayout from './layouts/Auth.js';
 
-import { AuthProvider } from './context/auth';
-
 function App() {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route
-					path='/admin'
-					render={(props) => (
-						<AuthProvider>
-							<AdminLayout {...props} />
-						</AuthProvider>
-					)}
-				/>
+				<Route path='/admin' render={(props) => <AdminLayout {...props} />} />
 				<Route path='/auth' render={(props) => <AuthLayout {...props} />} />
 				<Redirect from='/' to='/admin/index' />
 			</Switch>
